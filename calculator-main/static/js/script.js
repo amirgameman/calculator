3aa4bb800ffb827d2d3a0a12a771066f051eae6f
amirgameman <152512062+amirgameman@users.noreply.github.com>
2023-12-14 17:44:49 +0300

    Add files via upload

---
+++ calculator-main/static/js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener('click', processInput)
+    const char = {
+        '_seven': '7',
+        '_six': '6',
+        '_eight': '8',
+        '_nine': '9',
+        '_zero': '0',
+        '_one': '1',
+        '_two': '2',
+        '_three': '3',
+        '_four': '4',
+        '_five': '5',
+        '_divide': '/',
+        '_multiply': '*',
+        '_minus': '-',
+        '_plus': '+',
+        '_result': '=',
+        '_clear': 'clear',
+        '_backspace': 'backspace',
+    }
+
+    function processInput(event) {
+        if (event.target.closest('.key_digit')) {
+            let digitKey = event.target.closest('.key_digit').classList;
+            let digitKeyChar = digitKey[digitKey.length - 1];
+
+            let resChar = char[digitKeyChar];
+            console.log(resChar);
+        }
+        if (event.target.closest('.operation')) {
+            let digitKey = event.target.closest('.operation').classList;
+            let digitKeyChar = digitKey[digitKey.length - 1];
+
+            let resChar = char[digitKeyChar];
+            console.log(resChar);
+        }
+    }
+});
